1e1fb536e696648539012e28090703bf2125d282
Valentina98771 <76867909+Valentina98771@users.noreply.github.com>
2021-07-29 15:25:29 +0300

    Add files via upload

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,23 +1,23 @@
+from django.shortcuts import render
 from django.shortcuts import render, redirect
+# Create your views here.
+from .models import User
+from django.db import models
 from django.contrib import messages
-from accounts.models import NewUser
-from django.contrib.auth.models import User
-from django.contrib import auth
-from django.views.generic.edit import CreateView
 
 def RegisterUser(request):
     if request.method == "POST":
-        FirstName = request.POST.get("FirstName", "default value")
-        LastName = request.POST.get("LastName", "default value")
-        Username = request.POST.get("Username", "default value")
-        Email = request.POST.get("Email", "default value")
-        Password = request.POST["Password"]
-        Password1= request.POST["Password1"]
-        StatusUser = request.POST.get('StatusUser', 'default value')
-
-        if request.POST['Password'] == request.POST['Password1']:
-            NewUser(FirstName = FirstName, LastName = LastName, Username = Username, Email = Email, Password = Password, Password1 = Password1, StatusUser = StatusUser).save()
-            return render(request, "registration/index.html")
-        return render(request, "registration/register.html")
+        first_name = request.POST.get("first_name", "default value")
+        LastName = request.POST.get("last_name", "default value")
+        Username = request.POST.get("username", "default value")
+        Email = request.POST.get("email", "default value")
+        Password1 = request.POST.get("password", "default value")
+        Password2= request.POST.get("password1", "default value")
+        status = request.POST.get("status", 'default value')
+        #if request.POST.get("Password1") == request.POST.get("Password2"):
+        user = User(first_name = first_name, last_name = LastName, username = Username, email = Email, status = status)
+        user.set_password(Password1)
+        user.save()
+        return render(request, "registration/index.html")
     else:
-        return render(request, "registration/register.html")
+        return render(request, "registration/register.html", {})
