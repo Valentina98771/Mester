5328051621ce1ecbb83d221f5958a92d68ddc495
Valentina98771 <76867909+Valentina98771@users.noreply.github.com>
2021-07-29 15:26:25 +0300

    Add files via upload

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,14 +1,9 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class NewUser(models.Model):
-    FirstName = models.CharField(max_length=150)
-    LastName = models.CharField(max_length=150)
-    Username = models.CharField(max_length=150)
-    Email = models.EmailField(max_length=150)
-    Password = models.CharField(max_length=150)
-    Password1 = models.CharField(max_length=150)
-    StatusUser = models.CharField(max_length=150)
-    
-    USERNAME_FIELD = 'Username'
-    REQUIRED_FIELDS = []
-    
+from django.contrib.auth.models import AbstractUser, User
+
+class User(AbstractUser):
+    status = models.CharField(max_length=100)
