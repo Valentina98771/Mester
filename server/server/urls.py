f8acd9f036266210a51ef640344a3383309a53bb
Valentina98771 <76867909+Valentina98771@users.noreply.github.com>
2021-08-13 20:45:17 +0300

    Add files via upload

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,20 +1,37 @@
+"""server URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth_views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('posts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='registration/index.html'), name='index'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+    path('select/', TemplateView.as_view(template_name='registration/select.html'), name='select'),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),
-    path('profiles/', include('profiles.urls')),
-    path('profiles/', include('django.contrib.auth.urls')),
+    path('profileClient/', include('django.contrib.auth.urls')),
+    path('profileClient/', include('profileClient.urls')),
+    path('profile/', TemplateView.as_view(template_name='index.html'), name='index'),
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
