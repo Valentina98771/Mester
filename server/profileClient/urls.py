3b01f8009b8561c174c7a8e437f094f64b54c727
Valentina98771 <76867909+Valentina98771@users.noreply.github.com>
2021-08-26 11:36:55 +0300

    Add files via upload

--- server/profileClient/urls.py
+++ server/profileClient/urls.py
@@ -5,7 +5,11 @@ from django.views.generic.base import TemplateView
 app_name = 'profileClient'
 
 urlpatterns = [
-    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('index/', TemplateView.as_view(template_name='index.html'), name='index'),
     path('user/', views.UserEdit, name='user'),
+    path('about/', TemplateView.as_view(template_name='profile/about.html'), name='about'),
+    path('craftsmen/', TemplateView.as_view(template_name='profile/craftsmen.html'), name='craftsmen'),
+    path('photo/', TemplateView.as_view(template_name='profile/photo.html'), name='photo'),
+    path('posts/', TemplateView.as_view(template_name='profile/posts.html'), name='posts'),
 
 ]
