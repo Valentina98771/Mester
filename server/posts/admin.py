6c039514b8ac4bb0f96a02581598e74a8eb9a183
Valentina98771 <76867909+Valentina98771@users.noreply.github.com>
2021-08-27 15:32:41 +0300

    Add files via upload

---
+++ server/posts/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Post 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'body', 'created_on')
+    
+admin.site.register(Post, PostAdmin)
