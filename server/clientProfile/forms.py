3ce46b95295c506a9c0ff7f86c872408650b1320
Valentina98771 <76867909+Valentina98771@users.noreply.github.com>
2021-09-01 10:06:19 +0300

    Add files via upload

---
+++ server/clientProfile/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Profile
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username','first_name', 'last_name', 'email')
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
