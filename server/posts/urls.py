e9feae91b0836cbbf1a4ca7cb45af31ec7012c56
Valentina98771 <76867909+Valentina98771@users.noreply.github.com>
2021-09-03 17:03:24 +0300

    Add files via upload

--- server/posts/urls.py
+++ server/posts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import DeletePosts, DetailPosts, ListPosts, EditPosts
+from .views import DeletePost, DetailPost, ListPost, EditPost
 
 urlpatterns = [
-    path('addpost/', views.CreatePosts, name = "postare"),
-    path('home/', ListPosts.as_view(), name = "list"),
-    path('detail/<int:pk>/', DetailPosts.as_view(), name = 'detail'),
-    path('detail/edit/<int:pk>/', EditPosts.as_view(), name = 'edit'),
-    path('detail/<int:pk>/delete/', DeletePosts.as_view(), name = 'delete'),
+    path('addpost/', views.CreatePost, name = "postare"),
+    path('home/', ListPost.as_view(), name = "list"),
+    path('detail/<int:pk>/', DetailPost.as_view(), name = 'detail'),
+    path('detail/edit/<int:pk>/', EditPost.as_view(), name = 'edit'),
+    path('detail/<int:pk>/delete/', DeletePost.as_view(), name = 'delete'),
 
 ]
--- server/posts/views.py
+++ server/posts/views.py
@@ -4,29 +4,29 @@ from .models import Post
 from django.views.generic import ListView, UpdateView, DeleteView, DetailView
 from django.shortcuts import render, redirect
 
-def CreatePosts(request):
+def CreatePost(request):
     user = request.user
     body = request.POST.get("body", 'default value')
     created_on = request.POST.get("created_on", 'default value')
     updated_on = request.POST.get("updated_on", 'default value')
     post = Post.objects.create(user = user, body = body, created_on = created_on, updated_on = updated_on)
     post.save()
-    return redirect('list')
+    return redirect('postare')
 
-class ListPosts(ListView):
+class ListPost(ListView):
     model = Post
-    template_name = 'profile/profile.html'
+    template_name = 'profile/about.html'
 
-class DetailPosts(DetailView):
+class DetailPost(DetailView):
     model = Post
     template_name = 'postsDetail.html'
 
-class EditPosts(UpdateView):
+class EditPost(UpdateView):
     model = Post
     template_name = 'postsEdit.html'
     fields = ['body',]
 
-class DeletePosts(DeleteView):
+class DeletePost(DeleteView):
     model = Post
     template_name = 'postsDelete.html'
     success_url = reverse_lazy('list')
